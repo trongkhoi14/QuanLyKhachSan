c438713af67cda26a02376b36ac2817b59d2998d
20120514 <114341785+trongkhoi14@users.noreply.github.com>
2023-06-16 22:10:50 +0700

    create

---
+++ QuanLyKhachSan/QuanLyKhachSan/GUI/DangNhapGUI.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class DangNhapGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DangNhapGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "DangNhapGUI";
+            this.Text = "Đăng nhập";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
